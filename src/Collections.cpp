0c0843507ad21f6fe66470ec09e84a039e7a593a
samvaio <samvaiotmp@gmail.com>
2017-06-02 00:28:24 -0500

    version 1.1

--- src/Collections.cpp
+++ src/Collections.cpp
@@ -32,4 +32,44 @@ int main()
     }
     cout << endl;
 
+    cout << "int vector vi has " << vi.size() << " elements" << endl;
+
+    vi[5] = 3;
+    vi[6] = -1;
+    vi[1] = 99;
+
+    for(auto item:vi)
+    {
+        cout << item << " ";
+    }
+    cout << endl;
+
+    for(unsigned int i=0; i<vi.size(); i++)
+    {
+        cout << vi[i] << " ";
+    }
+    cout << endl;
+
+    for(auto i = begin(vi); i != end(vi); i++)
+    {
+        cout << *i << " ";
+    }
+    cout << endl;
+
+    sort(begin(vs),end(vs));
+
+    for(auto item:vs)
+    {
+        cout << item << " ";
+    }
+    cout << endl;
+
+    int threes = count(begin(vi),end(vi),3);
+    cout << "vector of ints has " << threes <<  " element 3s" << endl;
+
+    int tees = count(begin(vs[0]),end(vs[0]),'t');
+    cout << "first word has " << tees << " letter t's" << endl;
+
+    return 0;
+
 }
